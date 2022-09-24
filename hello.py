70fccff1d3c1e7ace7a18551ac8f5131f4b89b67
MELXIORY <83028802+melxiory@users.noreply.github.com>
2022-09-24 14:25:28 +0300

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,7 @@
+def app(environ, start_response):
+        body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+        start_response("200 OK", [
+            ("Content-Type", "text/plain"),
+            ("Content-Length", str(len(data)))
+        ])
+        return iter([data])
