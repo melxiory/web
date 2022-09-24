8300e26d37cd969cae534de39b7ffb294cce3ff6
MELXIORY <83028802+melxiory@users.noreply.github.com>
2022-09-24 14:38:23 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
   sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
   sudo rm -rf /etc/nginx/sites-enabled/default
   sudo /etc/init.d/nginx restart
+  sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
+
+  sudo /etc/init.d/gunicorn restart
+  gunicorn --bind='ip-адрес:порт' hello:hello
