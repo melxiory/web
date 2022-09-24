4353c50b7b482adc26fdda648c49ccf9977d14f7
MELXIORY <83028802+melxiory@users.noreply.github.com>
2022-09-24 15:57:09 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
 def application(environ, start_response):
-        body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
         start_response("200 OK", [
             ("Content-Type", "text/plain")
         ])
-        return ''.join(body)
+        return iter(["\n".join(environ.get('QUERY_STRING').split("&")).encode('utf-8')])
