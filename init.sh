7d83106dda18c2c93579f0a950621f457a30c1b0
MELXIORY <83028802+melxiory@users.noreply.github.com>
2022-11-09 19:55:08 +0300

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,8 @@
+  #!/bin/bash
+  
+  sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+  sudo rm -rf /etc/nginx/sites-enabled/default
+  sudo /etc/init.d/nginx restart
+  
+  cd ask/
+  sudo gunicorn --bind 0.0.0.0:8000 ask.wsgi:application&
