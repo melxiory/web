5cb431c0789ce380e82bd5a7cbdebc32c6232678
MELXIORY <83028802+melxiory@users.noreply.github.com>
2022-11-09 19:25:40 +0300

    Update views.py

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,4 +1,33 @@
-from django.http import HttpResponse 
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Question, Answer
+from .utils import paginate
 
+# Create your views here.
 def test(request, *args, **kwargs):
     return HttpResponse('OK')
+
+def main(request, *args, **kwargs):
+#    if request.GET.get('page') == None:
+#        request.GET['page'] = '1'
+    last = Question.objects.order_by('-added_at')
+    page = paginate(request,last)
+    return render(request, 'main.html', {
+        'questions': page.object_list,
+        'page': page,
+    })
+
+def popular(request):
+    pplr = Question.objects.popular()
+    page = paginate(request, pplr)
+    return render(request, 'main.html', {
+        'questions': page.object_list,
+        'page': page,
+    })
+
+def question(request, pk):
+    qst = get_object_or_404(Question, id=pk)
+    return render(request, 'question.html', {
+        'question': qst,
+        'answers': qst.answer_set.all()
+    })
